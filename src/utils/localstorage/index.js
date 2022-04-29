1198ff94e90ce4f00099303d03280da164cc5345
Orololuwa <ea@afarapartners.com>
2022-04-29 16:38:20 +0100

    localstoarge utils setup

---
+++ src/utils/localstorage/index.js
@@ -0,0 +1,15 @@
+class LocalStorage {
+    static get = (key) => {
+        if (typeof window !== 'undefined') return JSON.parse(localStorage.getItem(key));
+    }
+
+    static set = (key, value) => {
+        if (typeof window !== 'undefined') localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    static clear = (key) => {
+        if (typeof window !== 'undefined') window.localStorage.removeItem(key);
+    }
+}
+
+export default LocalStorage;
