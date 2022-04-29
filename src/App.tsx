4cf8a749b404f4bd0f23d2c3a33b4ac3d839d2df
Orololuwa <ea@afarapartners.com>
2022-04-29 12:10:48 +0100

    oauth setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getAuth, GithubAuthProvider, signInWithPopup } from "firebase/auth";
+import app from "./config/firebase-config";
+
+const auth = getAuth(app);
+const provider = new GithubAuthProvider();
 
 function App() {
+  const handleClick = async () => {
+    try {
+      const res = await signInWithPopup(auth, provider);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={handleClick}>github</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
