56482158713ea96e7df0543d88795549899bb94d
Orololuwa <ea@afarapartners.com>
2022-05-01 20:13:18 +0100

    aside component data from the api

--- src/store/user/userSlice.ts
+++ src/store/user/userSlice.ts
@@ -1,7 +1,42 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+interface UserData {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  name: string | null;
+  company: string | null;
+  blog: string;
+  location: string | null;
+  email: string | null;
+  hireable: boolean;
+  bio: string | null;
+  twitter_username: string | null;
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+  created_at: string;
+  updated_at: string;
+}
+
 interface UserState {
-  data: {} | undefined;
+  data: UserData | undefined;
 }
 
 const initialState: UserState = {
@@ -12,7 +47,7 @@ const UserSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser: (state, action: PayloadAction<{}>) => {
+    setUser: (state, action: PayloadAction<UserData>) => {
       state.data = action.payload;
     }
   }
