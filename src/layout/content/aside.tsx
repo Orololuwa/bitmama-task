5f34b3f7f255316e89bacc3c0505483d0c24d9ac
Orololuwa <ea@afarapartners.com>
2022-05-01 14:21:07 +0100

    desc in profile detail

--- src/layout/content/aside.tsx
+++ src/layout/content/aside.tsx
@@ -14,8 +14,9 @@ const Aside = (): JSX.Element => {
       </div>
       <div className="flex flex-col gap-4 my-4">
         <button className="bg-gray-100 hover:border-gray-400 border-[1px] border-gray-300 px-4 py-2 w-full rounded-md transition-colors">
-          Edit Profile
+          Follow
         </button>
+        <p>trying to be better than yesterday</p>
         <div className="flex items-center gap-1">
           <Icon icon="octicon:people-16" />
           <p className="font-bold mr-1">4</p>
